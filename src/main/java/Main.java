c79deb723b029a6cbd947c46527cc0d891e96a84
LorenzoGalimberti <lorenzogalimberti7996@gmail.com>
2023-09-07 12:05:24 +0200

    MILESTONE 4 COMPLETED

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class Main {
     // parametri di connessione al db
@@ -12,6 +13,17 @@ public class Main {
     private final static String DB_USER = "root";
     private final static String DB_PASSWORD = "root";
     // query da esguire
+    private final static String SQL_STATS= "select c.country_id, c.name as CountryName, group_concat(l.language) as Languages, s.year as LatestYear, s.population AS LatestPopulation, s.gdp as LatestGdp\n" +
+            "from country_languages cl\n" +
+            "join countries c on c.country_id = cl.country_id\n" +
+            "join languages l on l.language_id = cl.language_id\n" +
+            "join (\n" +
+            "    select country_id, max(year) as year\n" +
+            "    from country_stats\n" +
+            "    group by country_id\n" +
+            ") max_stats on c.country_id = max_stats.country_id\n" +
+            "join country_stats s on c.country_id = s.country_id and max_stats.year = s.year\n" +
+            "where c.country_id = ? group by c.country_id, c.name, s.year, s.population;";
     private final static String SQL_NATIONS = "select c.name as country_name  ,c.country_id as country_id, r.name as region_name , c2.name as continent_name\n" +
             "from countries c \n" +
             "join regions r on c.region_id  =r.region_id \n" +
@@ -52,7 +64,34 @@ public class Main {
                 System.out.println("Errore .");
             }
 
+        // QUERY PER LE STATISTICHE
+            System.out.print("Inserisci l 'ID della ricerca : ");
+            int choiche= scan.nextInt();
+            scan.nextLine();
+            try (PreparedStatement ps = conn.prepareStatement(SQL_STATS)){
+                ps.setInt(1,choiche);
+                try (ResultSet rs = ps.executeQuery()){
 
+                    while (rs.next()){
+                        int id= rs.getInt("country_id");
+                        String name = rs.getString("CountryName");
+                        String languages=rs.getString("Languages");
+                        int maxYear= rs.getInt("LatestYear");
+                        int latestPopulation= rs.getInt("LatestPopulation");
+                        double latestGdp= rs.getDouble("LatestGdp");
+
+                        System.out.println("Details for country : "+name);
+                        System.out.println("Languages : "+languages);
+                        System.out.println("Most recent stats ");
+                        System.out.println("Year : " +maxYear);
+                        System.out.println("Population : "+ latestPopulation);
+                        System.out.println("GDP : "+latestGdp);
+
+                    }
+                }
+            } catch (SQLException e){
+                System.out.println("Errore");
+            }
         } catch(SQLException e){
             System.out.println("OOPS an error occurred");
         }
